5277cc0e4b8f7287abe5beeab1a6fbe225da4737
Clement Escoffier <clement.escoffier@gmail.com>
2011-06-10 15:15:16 +0200

    Added license header.

--- roboject-it/src/main/java/de/akquinet/android/robojecttest/activities/InjectServiceTestActivity.java
+++ roboject-it/src/main/java/de/akquinet/android/robojecttest/activities/InjectServiceTestActivity.java
@@ -1,3 +1,17 @@
+/*
+
+This file is part of Roboject
+
+Copyright (c) 2010-2011 akquinet A.G.
+
+Contact:  http://www.akquinet.de/en
+
+GNU General Public License Usage
+This file may be used under the terms of the GNU General Public License version 3.0 as published by the Free Software Foundation and appearing in the file LICENSE included in the packaging of this file.  Please review the following information to ensure the GNU General Public License version 3.0 requirements will be met: http://www.gnu.org/copyleft/gpl.html.
+
+If you are unsure which license is appropriate for your use, please contact the sales department at http://www.akquinet.de/en.
+
+*/
 package de.akquinet.android.robojecttest.activities;
 
 import de.akquinet.android.roboject.RobojectActivity;
