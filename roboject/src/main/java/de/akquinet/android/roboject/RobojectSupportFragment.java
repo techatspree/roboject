0f8ca248fee46d187b5579cf67c66c6b368ec2bb
Enrico Biermann <Enrico@TMADell.(none)>
2012-02-07 16:30:43 +0100

    Reverted removal of reflection access to getActivity

--- roboject/src/main/java/de/akquinet/android/roboject/RobojectSupportFragment.java
+++ roboject/src/main/java/de/akquinet/android/roboject/RobojectSupportFragment.java
@@ -102,7 +102,18 @@ public class RobojectSupportFragment extends Fragment implements RobojectLifecyc
     private void createContainer() {
         if (this.container == null) {
             try {
-                this.container = new Container(getActivity(), this, getClass());
+                // Both the Android Support Library and ActionbarSherlock define
+                // getActivity() inside Fragment, but with different return types
+                // (FragmentActivity vs. Activity).
+                // That's why we call the method by reflection and cast the result to Activity.
+
+                Method getActivity = getClass().getMethod("getActivity", new Class[0]);
+                Object activity = getActivity.invoke(this, new Object[0]);
+                if (!(activity instanceof Activity)) {
+                    throw new RuntimeException("getActivity() returned null");
+                }
+
+                this.container = new Container((Activity) activity, this, getClass());
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
