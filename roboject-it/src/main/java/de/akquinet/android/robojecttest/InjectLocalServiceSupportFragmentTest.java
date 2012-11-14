206c750487228ea397f60e299011a58248117200
Philipp Kumar <philipp.kumar@gmail.com>
2012-11-14 01:41:03 +0100

    Added support for configuring objects as local services. Update to android-maven-plugin 3.4.0

---
+++ roboject-it/src/main/java/de/akquinet/android/robojecttest/InjectLocalServiceSupportFragmentTest.java
@@ -0,0 +1,46 @@
+/*
+
+This file is part of Roboject
+
+Copyright (c) 2010-2011 akquinet A.G.
+
+Contact:  http://www.akquinet.de/en
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package de.akquinet.android.robojecttest;
+
+import de.akquinet.android.marvin.ActivityTestCase;
+import de.akquinet.android.robojecttest.activities.DummySupportFragmentActivity;
+import de.akquinet.android.robojecttest.fragments.InjectNonAndroidServiceTestSupportFragment;
+
+import static org.hamcrest.CoreMatchers.*;
+
+
+public class InjectLocalServiceSupportFragmentTest extends ActivityTestCase<DummySupportFragmentActivity> {
+    public InjectLocalServiceSupportFragmentTest() {
+        super(DummySupportFragmentActivity.class);
+    }
+
+    public void testInjectLocalService() throws Exception {
+        Thread.sleep(2000);
+
+        InjectNonAndroidServiceTestSupportFragment fragment = (InjectNonAndroidServiceTestSupportFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.nonAndroidServiceSupportFragment);
+
+        assertThat(fragment.adder1, notNullValue());
+        assertThat(fragment.adder1.add(2, 3), is(equalTo(5)));
+
+        assertThat(fragment.adder2, notNullValue());
+        assertThat(fragment.adder2.add(2, 3), is(equalTo(5)));
+    }
+}
---
+++ roboject-it/src/main/java/de/akquinet/android/robojecttest/activities/InjectLocalServiceViaObjectTestActivity.java
@@ -0,0 +1,45 @@
+/*
+
+This file is part of Roboject
+
+Copyright (c) 2010-2011 akquinet A.G.
+
+Contact:  http://www.akquinet.de/en
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package de.akquinet.android.robojecttest.activities;
+
+import android.os.Bundle;
+import de.akquinet.android.roboject.RobojectActivity;
+import de.akquinet.android.roboject.ServiceRegistry;
+import de.akquinet.android.roboject.annotations.InjectService;
+import de.akquinet.android.robojecttest.services.AdderImplementation;
+import de.akquinet.android.robojecttest.services.AdderInterface;
+
+public class InjectLocalServiceViaObjectTestActivity extends RobojectActivity {
+    static {
+        ServiceRegistry.registerService(AdderInterface.class, new AdderImplementation());
+    }
+
+    @InjectService
+    public AdderInterface adder1;
+
+    public AdderInterface adder2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        adder2 = ServiceRegistry.getService(AdderInterface.class);
+    }
+}
