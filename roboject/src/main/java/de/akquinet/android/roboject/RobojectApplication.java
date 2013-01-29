49c5dcf89af1bfdf2c4b14de45f0e3ab72c18858
Enrico Biermann <enrico.biermann@akquinet.de>
2013-01-29 18:02:22 +0100

    Fixed use of RobojectApplication on older devices

--- roboject/src/main/java/de/akquinet/android/roboject/RobojectApplication.java
+++ roboject/src/main/java/de/akquinet/android/roboject/RobojectApplication.java
@@ -1,11 +1,14 @@
 package de.akquinet.android.roboject;
 
 import android.app.Application;
+import android.os.Build;
 
 public class RobojectApplication extends Application {
 
     public RobojectApplication() {
         super();
-        registerActivityLifecycleCallbacks(new RobojectContainer());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            registerActivityLifecycleCallbacks(new RobojectContainer());
+        }
     }
 }
